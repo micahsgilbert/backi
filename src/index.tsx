2f802a69188e82396b63dbf252d305f25d927673
Micah Gilbert <micahsgilbert@gmail.com>
2025-03-06 10:06:55 -0800

    homepage updates

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import backiConfig from './backi.yaml' 
 import Backi from './Backi'
-import { html } from '../README.md'
 
 import {BackiConfig} from './types'
 
@@ -14,7 +13,3 @@ createRoot(document.getElementById("backi")!).render(
   <Backi config={backiConfig as BackiConfig}/>
   </React.StrictMode>
 )
-
-window.addEventListener('load', () => {
-  document.getElementById("readme")!.innerHTML = html
-}) 
